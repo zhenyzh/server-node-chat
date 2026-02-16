af5dec47e1a5d43c1420f00dc201d26ca7657760
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-02-16 15:57:42 +0700

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import userRouter from "./user.router";
-import authRouter from "./auth.router";
+//import authRouter from "./auth.router";
 
 const router = Router();
 
-userRouter.get("/users", userRouter);
-router.use("/auth", authRouter);
+router.use("/users", userRouter);
+// router.use("/auth", authRouter);
 
 export default router;
--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -4,10 +4,10 @@ import { UserController } from "@/controllers/user.controllers";
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post("/registration", userController.registration);
-userRouter.post("/login", userController.login);
-userRouter.post("/logout", userController.logout);
-userRouter.get("/refresh", userController.refresh);
-userRouter.get("/users", userController.getUsers);
+// userRouter.post("/registration", userController.registration);
+// userRouter.post("/login", userController.login);
+// userRouter.post("/logout", userController.logout);
+// userRouter.get("/refresh", userController.refresh);
+userRouter.get("/", userController.getUsers);
 
 export default userRouter;
