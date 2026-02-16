b8c2a72a163fe6fb69c1d9708719b1115adda33f
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-02-16 13:04:11 +0700

    add confog dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+require('dotenv').config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World2332");
-});
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+const PORT = process.env.PORT || 5000;
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
